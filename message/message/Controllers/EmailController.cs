a9fcc5434202bfa888f7f73a4b0af540e2489ba1
Rao Atif <70482315+Atifba86@users.noreply.github.com>
2021-09-11 16:07:14 +0500

    1st commit

---
+++ message/message/Controllers/EmailController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+using System.Net;
+using System.Net.Mail;
+using System.Diagnostics;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace message.Controllers
+{
+    public class EmailController : Controller
+    {
+        // GET: Email
+
+        public ActionResult SendEmail()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SendEmail(string usermail)
+        {
+            string subject = "this is a subject part of mail";
+            string body = "this is a body part of mail";
+
+            WebMail.Send(usermail, subject, body, null, null);
+            @ViewBag.msg = "Email Sended !!";
+            return View();
+        }
+    }
+}
---
+++ message/message/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="message.MvcApplication" Language="C#" %>
